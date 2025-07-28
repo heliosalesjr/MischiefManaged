b40537f4927166e68f83dee32f235c055a98a445
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-07-28 11:58:42 -0300

    Layout and index.css adjustments for the house color to fill the bg

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children, currentView, onNavigate }) => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen ">
       
       <Navigation currentView={currentView} onNavigate={onNavigate} />
       
